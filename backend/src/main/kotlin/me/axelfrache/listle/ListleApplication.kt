e57f97cb3245986ef69499f1ef5334fea9e485fb
axelfrache <axel.frache@etu.umontpellier.fr>
2026-03-08 23:48:56 +0100

    feat: initial commit with backend

---
+++ backend/src/main/kotlin/me/axelfrache/listle/ListleApplication.kt
@@ -0,0 +1,11 @@
+package me.axelfrache.listle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ListleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ListleApplication>(*args)
+}
---
+++ backend/src/test/kotlin/me/axelfrache/listle/ListleApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.axelfrache.listle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ListleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
